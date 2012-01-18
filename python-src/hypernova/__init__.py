72cbab067116a43927b7022c909cc4e1f3fbe47c
Luke Carrier <luke.carrier@tdm.info>
2012-01-18 12:44:35 +0000

    Add the metadata and Distribute setup file

---
+++ python-src/hypernova/__init__.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3.2
+
+#
+# HyperNova server management framework
+#
+# Package (egg) definition
+#
+# Copyright (c) 2012 TDM Ltd
+#                    Luke Carrier <luke.carrier@tdm.info>
+#
+
+__author__        = 'The Development Manager Ltd'
+__author_email__  = 'luke.carrier@tdm.info'
+__description__   = 'HyperNova server agent'
+__friendly_name__ = 'HyperNova'
+__url__           = 'http://hypernova.cloudnova.net/'
+__version__       = '0.1.0'
---
+++ python-src/setup.py
@@ -0,0 +1,30 @@
+#!/usr/bin/env python3.2
+
+#
+# HyperNova server management framework
+#
+# Package (egg) definition
+#
+# Copyright (c) 2012 TDM Ltd
+#                    Luke Carrier <luke.carrier@tdm.info>
+#
+
+from setuptools import setup
+
+import hypernova as hn
+
+setup(
+    name         = hn.__friendly_name__,
+    version      = hn.__version__,
+    description  = hn.__description__,
+    author       = hn.__author__,
+    author_email = hn.__author_email__,
+    url          = hn.__url__,
+    packages     = ['hypernova'],
+    classifiers  = [
+        'Programming Language :: Python',
+        'Topic :: Internet'
+    ],
+    keywords     = 'cloud linux networking system',
+    license      = 'GPL'
+)
