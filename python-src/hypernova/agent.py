97921a9ff72602037c68329cf5a5a33ba20f4f12
Luke Carrier <luke.carrier@tdm.info>
2012-01-19 09:04:12 +0000

    Be PEP-8 compliant

--- python-src/hypernova/agent.py
+++ python-src/hypernova/agent.py
@@ -9,9 +9,9 @@
 #                    Luke Carrier <luke.carrier@tdm.info>
 #
 
+from http.server import HTTPServer, BaseHTTPRequestHandler
 import logging
 import logging.handlers
-from http.server import HTTPServer, BaseHTTPRequestHandler
 import sys
 
 if __name__ == '__main__':
@@ -23,21 +23,21 @@ class Agent:
     port = None
     log  = None
 
-    def __init__(self, addr = '0.0.0.0', port = 8080, log = '/tmp/hn-main.log'):
+    def __init__(self, addr='0.0.0.0', port=8080, log='/tmp/hn-main.log'):
         self.addr = addr
         self.port = port
         self.log  = log
 
     def execute(self):
-        self._initLogging()
+        self._init_logging()
 
         self._server = HTTPServer((self.addr, self.port), AgentRequestHandler)
-        self._log.info("entering server main loop")
+        self._log.info('entering server main loop')
         self._server.serve_forever()
-        self._log.info("server exiting")
+        self._log.info('server exiting')
 
 
-    def _initLogging(self):
+    def _init_logging(self):
         self._log_formatter = logging.Formatter(
             fmt = '[%(asctime)s] [%(levelname)-1s] %(message)s',
             datefmt = '%d/%m/%Y %I:%M:%S'
@@ -61,6 +61,12 @@ class AgentRequestHandler(BaseHTTPRequestHandler):
 
     def do_GET(self):
         self.send_response(500)
-        self.send_header('Content-Type', 'application/json')
         self.end_headers()
+
+    def do_POST(self):
+        #
+
+        self.send_response(result)
+        self.send_header('Content-Type', 'application/json')
+        self.end_headers
         self.wfile.write(b'<html><head><title>HyperNova</title></head><body><h1>HyperNova</h1><p>It\'s alive!</p></body></html>')
