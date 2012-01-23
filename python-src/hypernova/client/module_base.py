21ab140da3f99bf6c6e41b93b049e320dbd3ad3e
Luke Carrier <luke.carrier@tdm.info>
2012-01-23 09:10:00 +0000

    Stop Dave complaining about the agent crashing on startup (this will do something one day!)

---
+++ python-src/hypernova/client/module_base.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3.2
+
+#
+# HyperNova server management framework
+#
+# Base classes for modules
+#
+# Copyright (c) 2012 TDM Ltd
+#                    Luke Carrier <luke.carrier@tdm.info>
+#
+
+class BaseActionHandler:
+    pass
