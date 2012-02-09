801ac0f640c10bfb1c5a93a1626d6c17a7cf0135
Luke Carrier <luke.carrier@tdm.info>
2012-02-09 12:34:12 +0000

    Adding a test for permission elevation

---
+++ python-src/hypernova/modules/selftest.py
@@ -0,0 +1,39 @@
+#!/usr/bin/env python3.2
+
+#
+# HyperNova server management framework
+#
+# Self-test module
+#
+# Copyright (c) 2012 TDM Ltd
+#                    Luke Carrier <luke.carrier@tdm.info>
+#
+
+from hypernova.modules import AgentRequestHandlerBase
+from hypernova.libraries.configuration import ConfigurationFactory
+from hypernova.libraries.environment import where_is
+from hypernova.libraries.permissionelevation import elevate_cmd
+import subprocess
+
+class AgentRequestHandler(AgentRequestHandlerBase):
+
+    def do_elevation(params):
+
+        cmd    = elevate_cmd([where_is('whoami')])
+        target = ConfigurationFactory.get('hn-agent')['elevation']['target']
+        
+        try:
+            elevated_user = subprocess.check_output(cmd)
+        except subprocess.CalledProcessError:
+            elevated_user = None
+            
+        if elevated_user == target:
+            status = True
+        else:
+            status = False
+
+        result = {
+            "response": status,
+            "successful": status,
+        }
+        return AgentRequestHandlerBase._format_response(**result)
