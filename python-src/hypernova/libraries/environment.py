aefcad7f5fd302c46e732d91b3f2ff4a976adee5
Luke Carrier <luke.carrier@tdm.info>
2012-02-09 12:30:36 +0000

    Adding the ability to elevate permissions via the elevator tool

---
+++ python-src/hypernova/libraries/environment.py
@@ -0,0 +1,58 @@
+#!/usr/bin/env python3.2
+
+#
+# HyperNova server management framework
+#
+# System environment library
+#
+# Copyright (c) 2012 TDM Ltd
+#                    Luke Carrier <luke.carrier@tdm.info>
+#
+
+import os
+import platform
+
+def get_os():
+    """
+    Attempt to guess the OS type and release.
+
+    This hacky function attempts to guess the OS type based on the presence of a
+    few specific files.
+
+    Returns None of the OS name/version metadata cannot be found, else returns
+    a tuple containing the (name, version, release_title).
+    """
+
+    return platform.dist()
+
+def get_package_manager(releaseinfo):
+    """
+    Attempt to guess the package manager.
+    """
+
+    if releaseinfo[0].lower() in ('centos', 'fedora'):
+        return ('rpm', 'yum')
+
+    return None
+
+def where_is(binary, path=None):
+    """
+    Find a binary on a specified (or the system's path).
+
+    Seek a binary on the specified set of colon-separated paths. If this string
+    isn't set, default to the system's PATH environment variable. If this isn't
+    set either, try a sane default of /bin:/usr/bin.
+
+    Returns the path to the binary on success, None if it cannot be found.
+    """
+    if not path:
+        path = os.getenv('PATH', '/bin:/usr/bin')
+    paths = path.split(':')
+
+    for path in paths:
+        abs_path = os.path.join(path, binary)
+
+        if os.path.isfile(abs_path):
+            return abs_path
+
+    return None
---
+++ python-src/hypernova/libraries/permissionelevation.py
@@ -0,0 +1,39 @@
+#!/usr/bin/env python3.2
+
+#
+# HyperNova server management framework
+#
+# Permission elevation library
+#
+# Copyright (c) 2012 TDM Ltd
+#                    Luke Carrier <luke.carrier@tdm.info>
+#
+
+from hypernova.libraries.configuration import ConfigurationFactory
+
+def elevate_cmd(cmd):
+    """
+    Return an elevated command, ready for execution.
+
+    When cmd is a list, we prepend the options necessary for the command to run
+    as the system's root/administrative user as seperate, self-contained
+    arguments. When it's a string, we prepend to the string the space-delimited
+    parameters.
+
+    If the object is of any other type, a TypeError will be thrown.
+    """
+    
+    config = ConfigurationFactory.get('hn-agent')['elevation']
+    
+    if config['method'] == 'elevator':
+        prefix = [config['binary']]
+
+    if isinstance(cmd, list):
+        prefix.extend(cmd)
+        cmd = prefix
+    elif isinstance(cmd, str):
+        cmd = '%s %s' %(' '.join(prefix), cmd)
+    else:
+        raise TypeError('list or str expected')
+
+    return cmd
