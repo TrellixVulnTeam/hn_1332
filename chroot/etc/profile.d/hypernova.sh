c18fcae90220b6a3ff1d065e5be692bc8d7f3e64
Luke Carrier <luke.carrier@tdm.info>
2012-02-16 12:34:13 +0000

    Fixes #83 (again; this time some environment changes)

---
+++ chroot/etc/profile.d/hypernova.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env bash
+
+#
+# HyperNova server management framework
+#
+# Dirty great environment hacks
+#
+# Copyright (c) 2012 TDM Ltd
+#                    Luke Carrier <luke.carrier@tdm.info>
+#
+
+export PATH="$PATH:/usr/local/hypernova/bin"
