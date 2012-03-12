db17ccab20dab530a705d33fcc548b07c4cac3ee
Luke Carrier <luke.carrier@tdm.info>
2012-03-12 16:18:16 +0000

    Move the @Client@ library for the agent to an external module, so as to resolve potential cyclic dependency issues

--- src/hypernova/client.py
+++ src/hypernova/client.py
@@ -13,65 +13,11 @@ import argparse
 import configparser
 from http.client import HTTPConnection
 from hypernova import GPG
+from hypernova.libraries.client import Client
 from hypernova.libraries.configuration import ConfigurationFactory, LoadError
 import json
 import os
 import sys
-
-class Client:
-
-    addr = None
-    port = None
-
-    connection = None
-
-    M_GET  = 'GET'
-    M_POST = 'POST'
-
-    def __init__(self, addr='127.0.0.1', port=8080):
-
-        self.addr = addr
-        self.port = port
-
-        self._init_connection()
-        self._init_gpg()
-
-    def _init_connection(self):
-
-        self._connection = HTTPConnection(self.addr, self.port)
-
-    def _init_gpg(self):
-
-        self._gpg = GPG.get_gpg(gnupghome=os.path.join(os.getenv('HOME'),
-                                                       '.gnupg'))
-
-    def query(self, params, client_fp, server_fp):
-
-        if not isinstance(params, str):
-            params = json.dumps(params)
-
-        params = self._gpg.encrypt(params, server_fp, sign=client_fp)
-        encrypted_params = str(params)
-
-        if not params:
-            raise ValueError('Invalid passphrase, or the server\'s key has ' \
-                             'not been signed')
-
-        http_headers = {'Content-Length': len(encrypted_params)}
-        self._connection.request(self.M_GET, '/', body=encrypted_params,
-                                 headers=http_headers)
-
-        response = self._connection.getresponse()
-        response_data = str(response.read(), 'UTF-8')
-
-        response_data = self._gpg.decrypt(response_data)
-        if response_data.fingerprint != server_fp:
-            raise ValueError('Response was not signed')
-
-        self._connection.close()
-
-        return json.loads(str(response_data))
-
 
 class ClientActionBase:
     """
