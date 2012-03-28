7e68363947aac91e5ac37703ab5e6735751a04f8
Luke Carrier <luke.carrier@tdm.info>
2012-03-28 13:33:29 +0100

    Improves #254 (adds @dns.get_zone@ tests)

---
+++ test/functionals/test_dns.py
@@ -0,0 +1,36 @@
+#!/usr/bin/env python3.2
+
+#
+# HyperNova server management framework
+#
+# Agent DNS module unit tests
+#
+# Copyright (c) 2012 TDM Ltd
+#                    Luke Carrier <luke.carrier@tdm.info>
+#
+
+from functionals import ModuleFunctionalTestCase
+import re
+
+class TestGetZone(ModuleFunctionalTestCase):
+
+    def test_with_no_domain(self):
+
+        result = self.doRequest('dns', 'get_zone')
+
+        self.assertNonZero(result[0])
+
+        regex = re.compile('error: too few arguments')
+        self.assertIsNotNone(regex.search(result[2]))
+
+    def test_with_nonexistent_domain(self):
+
+        result = self.doRequest('dns', 'get_zone', {
+            'domain': 'nonexistent.domain',
+        })
+
+        self.assertNonZero(result[0])
+
+    def test_with_valid_domain(self):
+
+        
