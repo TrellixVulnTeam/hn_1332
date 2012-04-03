5948b56377930049bf99900c3b84fcd90d1d8ee4
Luke Carrier <luke.carrier@tdm.info>
2012-04-03 12:41:46 +0100

    Primitive @add_zone@ support in the client

--- src/hypernova/modules/dns.py
+++ src/hypernova/modules/dns.py
@@ -9,9 +9,12 @@
 #                    Luke Carrier <luke.carrier@tdm.info>
 #
 
-from hypernova.libraries.appconfig.dnsserver import NonexistentZoneError, \
+from hypernova.libraries.appconfig.dnsserver import DuplicateZoneError, \
+                                                    NonexistentZoneError, \
                                                     ServerCommunicationError, \
                                                     Record, \
+                                                    SoaRecord, \
+                                                    Zone, \
                                                     get_authoritative_server
 from hypernova.libraries.configuration import ConfigurationFactory
 from hypernova.libraries.packagemanagement import get_package_db, \
@@ -29,6 +32,42 @@ class AgentRequestHandler(AgentRequestHandlerBase):
         """
         Add a zone.
         """
+
+        config = ConfigurationFactory.get('hypernova')
+
+        try:
+            result = {'error': 'ValidationError'}
+
+            server = get_authoritative_server(config['dns']['adapter'],
+                                              config['dns'])
+            zone = Zone(new_domain=params['zone']['domain'],
+                        new_origin=params['zone']['origin'],
+                        new_ttl=params['zone']['ttl'])
+            zone.soa_record = SoaRecord(new_primary_ns=params['soa']['primary_ns'],
+                                        new_responsible_person=params['soa']['responsible_person'],
+                                        new_serial=params['soa']['serial'],
+                                        new_refresh=params['soa']['refresh'],
+                                        new_retry=params['soa']['retry'],
+                                        new_expire=params['soa']['expire'],
+                                        new_min_ttl=params['soa']['min_ttl'])
+
+            try:
+                server.add_zone(zone)
+                successful = True
+                result = {'zone': zone.to_encodable()}
+            except DuplicateZoneError:
+                successful = False
+                result = {'error': 'DuplicateZone'}
+
+        except ServerCommunicationError:
+            successful = False
+            result = {'error': 'ServerCommunicationError'}
+
+        return AgentRequestHandler._format_response(
+            result,
+            successful=successful,
+            error_code=0
+        )
 
     def do_get_zone(params):
         """
@@ -80,24 +119,59 @@ class ClientRequestBuilder(ClientRequestBuilderBase):
     DNS management request generation component.
     """
 
+    # ints must be explicitly casted from str
+    ZONE_ATTR_INT = ['origin', 'ttl']
+    ZONE_ATTR_STR = ['domain']
+    SOA_ATTR_INT  = ['serial', 'refresh', 'retry', 'expire', 'min_ttl']
+    SOA_ATTR_STR  = ['primary_ns', 'responsible_person']
+
     def init_subparser(subparser, subparser_factory):
         sp = subparser_factory.add_parser('add_zone')
-        sp.add_argument('domain')
+
+        for a in (ClientRequestBuilder.ZONE_ATTR_STR +
+                  ClientRequestBuilder.ZONE_ATTR_INT):
+            sp.add_argument(a)
+        for a in (ClientRequestBuilder.SOA_ATTR_STR +
+                  ClientRequestBuilder.SOA_ATTR_INT):
+            sp.add_argument("soa_%s" %(a))
+
         sp = subparser_factory.add_parser('get_zone')
         sp.add_argument('domain')
 
         subparser_factory.add_parser('install')
+
         return subparser
 
     def do_add_zone(cli_args, client):
-        return ClientRequestBuilderBase._format_request(
-            ['dns', 'add_zone'],
-            {
+        """
+        Add a zone.
+        """
 
-            }
+        args = { 'zone': {}, 'soa': {} }
+
+        for a in ClientRequestBuilder.ZONE_ATTR_INT:
+            args['zone'][a] = int(getattr(cli_args, a))
+        for a in ClientRequestBuilder.ZONE_ATTR_STR:
+            args['zone'][a] = getattr(cli_args, a)
+
+        for a in ClientRequestBuilder.SOA_ATTR_INT:
+            args['soa'][a] = int(getattr(cli_args, "soa_%s" %(a)))
+        for a in ClientRequestBuilder.SOA_ATTR_STR:
+            args['soa'][a] = getattr(cli_args, "soa_%s" %(a))
+
+        for a in ['origin', 'ttl']:
+            if args['zone'][a] == -1:
+                args['zone'][a] = None
+
+        return ClientRequestBuilderBase._format_request(
+            ['dns', 'add_zone'], args
         )
 
     def do_get_zone(cli_args, client):
+        """
+        Retrieve a zone.
+        """
+
         return ClientRequestBuilderBase._format_request(
             ['dns', 'get_zone'], {
                 'domain': cli_args.domain
@@ -105,6 +179,10 @@ class ClientRequestBuilder(ClientRequestBuilderBase):
         )
 
     def do_install(cli_args, client):
+        """
+        Install a DNS server.
+        """
+
         return ClientRequestBuilderBase._format_request(
             ['dns', 'install']
         )
@@ -116,9 +194,11 @@ class ClientResponseFormatter(ClientResponseFormatterBase):
     """
 
     errors = {
+        'DuplicateZone': 'a zone with the specified domain already exists',
         'NonexistentZone': 'the specified zone does not exist',
         'ServerCommunication': 'could not communicate with the DNS server',
         'UnknownError': 'an unknown error occurred within the agent',
+        'ValidationError': 'a zone or record failed validation checks and couldn\'t be committed',
     }
 
     DIRECTIVES_FMT = "Directives:\n* Domain: %s\n* TTL: %s\n* Origin: %s"
@@ -185,6 +265,26 @@ class ClientResponseFormatter(ClientResponseFormatterBase):
             ClientResponseFormatter._format_records(zone['records']),
         ])
 
+    def do_add_zone(cli_args, response):
+        """
+        Add a zone.
+        """
+
+        result = "Failed: %s"
+
+        if response['status']['successful']:
+            zone = response['response']['zone']
+            result = ClientResponseFormatter._format_zone(zone)
+        else:
+            try:
+                seeking = response['response']['error']
+            except KeyError:
+                seeking = 'UnknownError'
+
+            return (69, result %(ClientResponseFormatter.errors[seeking]))
+
+        return result
+
     def do_get_zone(cli_args, response):
         """
         Retrieve a zone.
@@ -194,7 +294,6 @@ class ClientResponseFormatter(ClientResponseFormatterBase):
 
         if response['status']['successful']:
             zone = response['response']['zone']
-
             return ClientResponseFormatter._format_zone(zone)
         else:
             try:
