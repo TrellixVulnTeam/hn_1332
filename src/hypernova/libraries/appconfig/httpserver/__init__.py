d56a8613febe9f9546dbcbc10c1069d2863932bc
Luke Carrier <luke.carrier@tdm.info>
2012-04-05 11:11:37 +0100

    Import submodules into the @httpserver@ module at runtime, since we're going to make it easier to obtain system configuration instances from within the @provisioner@.

--- src/hypernova/libraries/appconfig/httpserver/__init__.py
+++ src/hypernova/libraries/appconfig/httpserver/__init__.py
@@ -46,3 +46,15 @@ class VirtualHostBase(AppConfigBase):
     #
     # ['php'] = True|False
     dynamic_langs = {}
+
+# Import submodules
+#
+# Since __all__ only contains imported submodules, we need to walk through the
+# packages under this namespace and import them here in order to work with them
+# later. This isn't pretty, but it works.
+__all__ = []
+for (loader, module_name, is_package) in pkgutil.walk_packages(__path__):
+    __all__.append(module_name)
+    module = loader.find_module(module_name).load_module(module_name)
+    exec("%s = module" %(module_name))
+
