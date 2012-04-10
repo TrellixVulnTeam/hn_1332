dd05a39002e2ff878abd78eec032ab48cc1e31e0
Luke Carrier <luke.carrier@tdm.info>
2012-04-10 17:52:34 +0100

    Changes to the @httpserver@ module to make it easier to configure and a lot more powerful

--- src/hypernova/libraries/appconfig/httpserver/__init__.py
+++ src/hypernova/libraries/appconfig/httpserver/__init__.py
@@ -11,11 +11,55 @@
 
 from hypernova.libraries.appconfig import AppConfigBase
 import pkgutil
+import sys
 
 class HttpServerConfigBase(AppConfigBase):
+    """
+    Base representation of an HTTP server's configuration.
+
+    Note: these classes are strictly one-way only -- no attempts are made to
+          load existing configuration. Such attempts are unreliable and by no
+          means suited to use in production environments.
+    """
+
+    def __init__(self, config_dir, dir_mapping=None):
+        """
+        Initialise an HTTP server configuration instance.
+
+        The configuration directory is the root directory of the webserver's
+        configuration, usually /etc/somewhere. The mapping parameters should be
+        either None or a dictionary containing all or some of the following
+        keys, as is necessary in the given environment:
+
+          * virtualhosts - the directory to store virtual hosts in.
+
+        The defaults for the given web server will be assumed if not present in
+        the mapping dictionary.
+        """
+
+        pass
+
+    def commit_virtualhost(self, vhost):
+        """
+        Commit a changed virtualhost.
+        """
+
+        pass
 
     def get_virtualhost(self):
+        """
+        Create a new virtualhost.
+        """
+
         pass
+
+    def has_virtualhost(self, vhost):
+        """
+        Does this server object have a vhost with that name?
+        """
+
+        pass
+
 
 class VirtualHostBase(AppConfigBase):
     """
@@ -26,7 +70,7 @@ class VirtualHostBase(AppConfigBase):
     #
     # [0] = network
     # [1] = port
-    listen_socket = []
+    listen = []
 
     # Server names
     #
@@ -43,10 +87,74 @@ class VirtualHostBase(AppConfigBase):
     # Automatically index directory contents?
     auto_index = False
 
-    # Dynamic languages
+    # External configuratrion file includes
     #
-    # ['php'] = True|False
-    dynamic_langs = {}
+    # If unsupported by the web server itself, these should be handled at
+    # write-time, as they're an essential part of configuration.
+    #
+    # Except in those servers which don't natively support an include
+    # directive, these files are assumed to be relative to the server's
+    # configuration root directory. You should consult the documentation for
+    # specific modules if you suspect this path is wrong.
+    includes = []
+
+    # Location blocks
+    #
+    # The concept is based on that of nginx:
+    #    http://wiki.nginx.org/HttpCoreModule#location
+    locations = []
+
+    def add_location(self):
+        """
+        Add a location block to the stack.
+        """
+
+        Klass = getattr(sys.modules[__name__], 'VirtualHost')
+        location = self.locations.append(Klass())
+        return location
+
+    def is_valid(self):
+        """
+        Is the vhost semantically valid?
+
+        This will always be a best guess attempt and will never be truly in line
+        with the validations of the server, but should provide a good indication
+        when dire things are afoot.
+        """
+
+        pass
+
+
+class VirtualHostLocationBase(AppConfigBase):
+    """
+    Virtual host location object.
+    """
+
+    MODES = [
+        'exact',
+        'regex',
+    ]
+
+    # Location
+    uri = ''
+
+    # Match mode
+    mode = ''
+
+
+class InvalidVirtualHostError(Exception):
+    """
+    Thrown when a commit is attempted on an invalid virtual host.
+    """
+
+    pass
+
+def get_server(server):
+    """
+    Get an HTTP server configuration instance.
+    """
+
+    return getattr(globals()[server], 'AppConfig')
 
 # Import submodules
 #
--- src/hypernova/libraries/appconfig/httpserver/nginx.py
+++ src/hypernova/libraries/appconfig/httpserver/nginx.py
@@ -3,7 +3,8 @@
 #
 # HyperNova server management framework
 #
-# nginx configurator
+# Templates, utility functions and class definitions to represent and generate
+# nginx configuration files.
 #
 # Copyright (c) 2012 TDM Ltd
 #                    Luke Carrier <luke.carrier@tdm.info>
@@ -11,58 +12,93 @@
 
 from hypernova.libraries.appconfig.httpserver import HttpServerConfigBase, \
                                                      VirtualHostBase
+from os.path import abspath, isfile, join
+
+# Global blocks
+templ_block_events   = """events {
+    %s
+}"""
+templ_block_http     = """http {
+    %s
+}"""
+templ_block_location = """location {
+    %s
+}"""
+templ_block_server   = """server {
+    %s
+}"""
+templ_block_types    = """types {
+    %s
+}"""
+
+# Global directives
+templ_directive_daemon           = "daemon %s;"
+templ_directive_default_type     = "default_type %s;"
+templ_directive_include          = "include %s;"
+templ_directive_user             = {1: "user %s;", 2: "user %s %s;"}
+templ_directive_worker_processes = "worker_processes %i;"
+
+# events {  } directives
+templ_directive_use                = "use %s;"
+templ_directive_worker_connections = "worker_connections %i;"
+
+# http { server {  } } directives
+templ_directive_auto_index         = "autoindex %s;"
+templ_directive_indexes           = "index %s;"
+templ_directive_keepalive_timeout = "keepalive_timeout %s;"
+templ_directive_listen            = "listen %s;"
+templ_directive_document_root     = "root %s;"
+templ_directive_sendfile          = "sendfile %s;"
+templ_directive_server_names      = "server_name %s;"
+
+def to_nginx_bool(self, bool_val):
+    """
+    Convert to nginx boolean.
+
+    nginx uses on and off instead of true and false, so we account for that
+    here by converting Python bool()s into nginx booleans.
+    """
+
+    if bool_val:
+        return 'on'
+    else:
+        return 'off'
 
 class AppConfig(HttpServerConfigBase):
     """
     nginx configurator.
     """
 
+    def __init__(self, config_dir, dir_mapping=None):
+        if not dir_mapping:
+            dir_mapping = {}
+
+        if 'virtualhosts' not in dir_mapping.keys():
+            dir_mapping['virtualhosts'] = 'sites'
+
+        self.config_dir = abspath(config_dir)
+        self.vhost_dir  = join(self.config_dir, dir_mapping['virtualhosts'])
+
+    def commit_virtualhost(self, vhost):
+        if not vhost.is_valid():
+            raise InvalidVirtualHostError()
+
+        with open(join(self.vhost_dir, vhost.server_names[0]), 'w') as f:
+            f.write(str(vhost))
+
     def get_virtualhost(self):
         return VirtualHost()
+
+    def has_virtualhost(self, vhost):
+        return isfile(join(self.vhost_dir, vhost.server_names[0]))
+
 
 class VirtualHost(VirtualHostBase):
     """
     nginx virtualhost.
     """
 
-    # Virtual host template
-    __templ = """
-server {
-    listen %s:%s;
-    server_name %s;
-
-    root %s;
-
-    index %s;
-    auto_index %s;
-
-    %s
-}
-"""
-
-    def __str__(self):
-        return self.__templ %(self.listen_socket[0],
-                              self.listen_socket[1],
-                              ' '.join(self.server_names),
-                              self.document_root,
-                              ' '.join(self.indexes),
-                              self.__to_nginx_bool(self.auto_index),
-                              self.__to_nginx_includes(self.dynamic_langs))
-
-    def __to_nginx_bool(self, bool_val):
-        """
-        Convert to nginx boolean.
-
-        nginx uses on and off instead of true and false, so we account for that
-        here by converting Python bool()s into nginx booleans.
-        """
-
-        if bool_val:
-            return 'on'
-        else:
-            return 'off'
-
-    def __to_nginx_includes(self, dynamic_langs):
+    def fmt_directive_includes(self, includes):
         """
         Convert the dynamic languages dictionary into nginx configuration file
         includes.
@@ -70,10 +106,48 @@ server {
 
         opts = []
 
-        if not 'php' in dynamic_langs:
-            dynamic_langs = False
+        for i in includes:
+            opts.append(globals()['templ_directive_include'] %(i))
 
-        if dynamic_langs['php']:
-            opts.append('   include enable_php;');
+        return "\n    ".join(opts)
 
-        return '\n'.join(opts)
+    def fmt_directive_server_names(self, server_names):
+        """
+        Format a list of server names for a configuration file.
+        """
+
+        return globals()['templ_directive_server_names'] %(' '.join(server_names))
+
+    def is_valid(self):
+        # TODO: validation
+        return True
+
+    def __str__(self):
+        templates  = globals()
+        directives = ['auto_index', 'document_root', 'includes', 'indexes',
+                      'listen', 'server_names']
+        blocks     = ['location']
+        chunks     = []
+
+        # Format each individual directive.
+        #
+        # Attempt to find fmt_directive_*() methods to handle the translation of
+        # special directives which cannot follow the conventional use of a
+        # format string. Else, look for a global templ_directive_* variable to
+        # use as a format string.
+        #
+        # TODO: add support for location blocks and fancier directives.
+        for d in directives:
+            value = getattr(self, d, None)
+
+            if not value:
+                continue
+
+            try:
+                chunk = getattr(self, 'fmt_directive_' + d)(value)
+            except AttributeError:
+                chunk = templates['templ_directive_' + d] %(value)
+            chunks.append(chunk)
+
+        return templates['templ_block_server'] %("\n    ".join(chunks))
+
