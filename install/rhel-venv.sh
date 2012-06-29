5660e662d8fc46226b17682ed5910893b56eb698
Luke Carrier <hey@lukecarrier.me>
2012-06-29 17:13:01 +0100

    Add Pythonbrew support to the virtualenv installer to workaround #496
    * Modify the installer to detect a Pythonbrew installation and use its integrated venv support if available
    * Create symbolic links to any of the files the virtualenv module would usually create to remain compatible with existing shell scripts (wrapper applications in source:chroot/bin)
    * Use the @python3.2@ on @$PATH@ throughout the installation

--- install/rhel-venv.sh
+++ install/rhel-venv.sh
@@ -22,10 +22,24 @@ in_array() {
 
 # Exit the running script with an error status and clear warning
 error_trap() {
-    echo " "
-    echo "$0: an error occurred during the execution of an action; aborting" >&2
-    echo " "
-    exit 69
+    if [ "$EXIT_ON_ERROR" = "1" ]; then
+        echo " "
+        echo "$0: an error occurred during the execution of an action; aborting" >&2
+        echo " "
+        exit 69
+    else
+        echo "$0: being tolerant of possible error condition" >&2
+    fi
+}
+
+# Enable error trapping (die on error)
+trap_on() {
+    EXIT_ON_ERROR=1
+}
+
+# Disable error trapping
+trap_off() {
+    EXIT_ON_ERROR=0
 }
 
 # Establish what we need to do before initiating error trapping, since we have
@@ -47,7 +61,7 @@ if [ -z "$VIRTUAL_ENV" ] && [ $FORCE -eq 0 ]; then
 fi
 
 # Don't risk screwing the system up; exit early
-trap error_trap 1 2 3 15 ERR
+trap_on
 
 # Root of the source tree
 srcroot="$(readlink -fn "$(dirname "$0")/..")"
@@ -56,24 +70,54 @@ cd $srcroot
 # If forced, remove all the things
 if [ "$FORCE" == 1 ]
 then
-    rm -rf $srcroot/chroot/bin/{activate*,easy_install*,pip*,py*}
+    rm -rf $srcroot/chroot/bin/{activate,easy_install,pip,py}*
+    pythonbrew venv delete hypernova
 fi
 
 # Prepare the system
 #   For convenience, these can be skipped when you're running them from your
 #   IDE, so you won't need to stay elevated or keep bashing out your password!
-if [ "$SKIPDEPS" != 1 ]; then
+if [ "$SKIPDEPS" = 0 ]; then
     # Install system packages
-    sudo yum -y install bash curl git python3{,-devel,-libs}
+    sudo yum -y install bash curl git
 
-    # Set up setuptools/distribute
-    pushd /tmp
-    sudo python3.2 < <(curl -s http://python-distribute.org/distribute_setup.py)
-    rm -f distribute_setup.py
-    popd
+    # Disable error trapping while we process options
+    trap_off
 
-    # Set up virtualenv
-    sudo easy_install-3.2 virtualenv
+    # Bail if Python 3.2 isn't available on $PATH
+    which python3.2 >/dev/null
+    retval="$?"
+    if [ "$?" != "0" ]; then
+        echo "python3.2 must be available on your PATH!"
+        exit 1
+    fi
+
+    # Detect pythonbrew; we can sidestep the system python totally then
+    USE_PYTHONBREW=0
+    in_array "--use-pythonbrew" "${@}"
+    retval="$?"
+    if [ "$retval" = 0 ] || [[ "$PYTHONPATH" =~ "$HOME/.pythonbrew" ]]; then
+        echo "pythonbrew detected or forced; we'll assume a per-user install"
+        USE_PYTHONBREW=1
+    fi
+
+    # Re-enable error trapping
+    trap_on
+
+    # Set up setuptools/distribute and virtualenv
+    if [ "$USE_PYTHONBREW" = 0 ]; then
+        pushd /tmp
+        sudo python3.2 < <(curl -s http://python-distribute.org/distribute_setup.py)
+        rm -f distribute_setup.py
+        popd
+
+        sudo easy_install-3.2 virtualenv
+    else
+        pythonbrew switch 3.2.3
+        pythonbrew venv init
+        pythonbrew venv create hypernova
+        pythonbrew venv use hypernova
+    fi
 
     # Set up elevator
     pushd deps/elevator
@@ -89,16 +133,25 @@ if [ "$SKIPDEPS" != 1 ]; then
 fi
 
 # Set up virtualenv
-if [ "$FORCE" == 1 ] || [ -f bin/activate ]; then
-    pushd chroot
-    [ ! -f "bin/activate" ] && virtualenv --no-site-packages .
-    . bin/activate
-    popd
+if [ "$FORCE" == 1 ] || [ ! -f bin/activate ]; then
+    if [ "$USE_PYTHONBREW" = "0" ]; then
+        pushd chroot
+        if [ ! -f "bin/activate" ]; then
+            virtualenv --help
+            virtualenv "$(pwd)"
+        fi
+        . bin/activate
+        popd
+    else
+        touch "$srcroot/chroot/bin/activate"
+        ln -s "$(which python3.2)" "$srcroot/chroot/bin/python3.2"
+    fi
 
     # Patch virtualenv activation file
     echo "
     export BINDIR="$srcroot/chroot/bin"
     export CONFDIR="$srcroot/chroot/etc/hypernova"
+    export PATH="\$PATH:\$BINDIR"
     " >> "$srcroot/chroot/bin/activate"
 fi
 
@@ -107,35 +160,35 @@ if [ "$SKIPDEPS" != 1 ]; then
     # Set up pexpect
     pushd deps/python-pexpect
     rm -rfv build/ dist/
-    "$srcroot/chroot/bin/python3.2" setup.py bdist_egg
-    "$srcroot/chroot/bin/easy_install-3.2" dist/pexpect-*-py3.2.egg
+    "python3.2" setup.py bdist_egg
+    "easy_install-3.2" dist/pexpect-*-py3.2.egg
     popd
 
     # Set up gnupg
     pushd deps/python-gnupg
     rm -rfv build/ dist/
-    "$srcroot/chroot/bin/python3.2" setup.py bdist_egg
-    "$srcroot/chroot/bin/easy_install-3.2" dist/python_gnupg-*-py3.2.egg
+    "python3.2" setup.py bdist_egg
+    "easy_install-3.2" dist/python_gnupg-*-py3.2.egg
     popd
 
     # Set up oursql
     pushd deps/python-oursql
     rm -rfv build/ dist/
-    "$srcroot/chroot/bin/python3.2" setup.py bdist_egg
-    "$srcroot/chroot/bin/easy_install-3.2" dist/oursql-*-py3.2-linux-$(arch).egg
+    "python3.2" setup.py bdist_egg
+    "easy_install-3.2" dist/oursql-*-py3.2-linux-$(arch).egg
     popd
 
     # Set up pyrg
     pushd deps/python-pyrg
     rm -rfv build/ dist/
-    "$srcroot/chroot/bin/python3.2" setup.py bdist_egg
-    "$srcroot/chroot/bin/easy_install-3.2" dist/pyrg-*-py3.2.egg
+    "python3.2" setup.py bdist_egg
+    "easy_install-3.2" dist/pyrg-*-py3.2.egg
     popd
 fi
 
 # Install HyperNova
 pushd src
 rm -rfv build/ dist/
-"$srcroot/chroot/bin/python3.2" setup.py bdist_egg
-"$srcroot/chroot/bin/easy_install-3.2" dist/HyperNova-*-py3.2.egg
+"python3.2" setup.py bdist_egg
+"easy_install-3.2" dist/HyperNova-*-py3.2.egg
 popd
