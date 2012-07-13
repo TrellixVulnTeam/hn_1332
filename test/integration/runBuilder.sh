eb1ee205d892b0ce333e46d669a2a3445d935f68
Laurent David <laurent@tdm.info>
2012-07-13 15:01:35 +0100

    Builder is now working

--- test/integration/runBuilder.sh
+++ test/integration/runBuilder.sh
@@ -1,10 +1,10 @@
 #!/bin/sh
 [ "X`which qemu-system-x86_64`" = "X" ] && echo "qemu-system-x86_64  should be installed on your system" && exit 0
-[ ! -e centos-6.0.img ] && scp dev.ossservices.com:/srv/centos-6.0.img.gz . && echo "Downloading the image" && gunzip centos-6.0.img.gz
+[ ! -e centos-6.2.img ] && scp dev.ossservices.com:/srv/centos-6.2.img.gz . && echo "Downloading the image" && gunzip centos-6.2.img.gz
 
 
 # Run the build system
-[ ! -e builder.img ] && qemu-img create -f qcow2 -b centos-6.0.img builder.img && echo "Creating the image..." 
+[ ! -e builder.img ] && qemu-img create -f qcow2 -b centos-6.2.img builder.img && echo "Creating the image..." 
 
 echo "Launch the VM to build HyperNova Package: will be use for the build AND the client"
 echo "Running emulator. You can ssh into it from the port 3333"
