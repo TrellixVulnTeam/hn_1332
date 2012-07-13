803d898086e2994d340e0fdc0e3d73e08345dfa3
Laurent David <laurent@tdm.info>
2012-07-13 15:32:08 +0100

    Copy the end results on the local folder

--- test/integration/runBuilder.sh
+++ test/integration/runBuilder.sh
@@ -4,17 +4,20 @@
 
 
 # Run the build system
-[ ! -e builder.img ] && qemu-img create -f qcow2 -b centos-6.2.img builder.img && echo "Creating the image..." 
+[ ! -e manager.img ] && qemu-img create -f qcow2 -b centos-6.2.img manager.img && echo "Creating the manager image..." 
+[ ! -e node.img ] && qemu-img create -f qcow2 -b centos-6.2.img node.img && echo "Creating the node image..." 
 
 echo "Launch the VM to build HyperNova Package: will be use for the build AND the client"
 echo "Running emulator. You can ssh into it from the port 3333"
-qemu-system-x86_64 -m 512 -net nic -net user -hda builder.img  -enable-kvm -redir tcp:3333::22 &
+qemu-system-x86_64 -m 512 -net nic,vhost -net user -hda manager.img  -enable-kvm &
+qemu-system-x86_64 -m 512 -net nic -net user -hda node.img  -enable-kvm &
 
 until `ssh -l hnbuild localhost -p 3333 exit` ; do echo -n "."; done
 
 echo "Running the build"
 ../../install/rhel-push.sh -H localhost -U hnbuild -P 3333
-
+# Copy the result
+scp -P 3333 -r hnbuild@localhost:/home/hnbuild/hypernova/install/build/RPMS/ .
 # Run the target server system
 #echo "Launch the VM as a target install for HyperNova"
 #qemu-img create -f qcow2 -b centos-6.0.img builder.img 
