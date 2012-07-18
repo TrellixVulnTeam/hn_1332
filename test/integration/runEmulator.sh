6b827ac62b0393ef28ddcc14dc94b15eccaac1f9
Laurent David <laurent@tdm.info>
2012-07-18 23:21:58 +0100

    Still issues with guest images

--- test/integration/runEmulator.sh
+++ test/integration/runEmulator.sh
@@ -9,8 +9,8 @@
 
 echo "Running emulator. You can ssh into it from the port 3333"
 # For Mac address see /etc/udev rules, we have 2 interfaces eth0=..66 and eth1=...56
-qemu-system-x86_64 -m 512 -net nic,macaddr=52:54:00:f5:80:66  -net socket,listen=:6070  -net nic,vlan=2 -net user,vlan=2 -redir tcp:3333::22 -hda manager.img  -enable-kvm  &
-qemu-system-x86_64 -m 512 -net nic,macaddr=52:54:00:12:34:56  -net socket,connect=127.0.0.1:6070 -net nic,vlan=2 -net user,vlan=2 -redir tcp:4444::22 -hda node.img  -enable-kvm &
+qemu-system-x86_64 -m 512 -net nic,model=ne2k_pci,macaddr=52:54:00:f5:80:66  -net socket,listen=:6070  -net nic,model=ne2k_pci -net user -redir tcp:3333::22 -hda manager.img  -enable-kvm  &
+qemu-system-x86_64 -m 512 -net nic,model=ne2k_pci,macaddr=52:54:00:12:34:56  -net socket,connect=127.0.0.1:6070  -hda node.img  -enable-kvm &
 
 #qemu-system-x86_64 -m 512 -net nic,macaddr=52:54:00:f5:80:66 -net user,dhcpstart=10.0.2.15 -redir tcp:3333::22 -net socket,listen=:6070 -hda manager.img  -enable-kvm  &
 #qemu-system-x86_64 -m 512 -net nic,macaddr=52:54:00:12:34:56 -net user -redir tcp:4444::22  -net socket,connect=127.0.0.1:6070 -hda node.img  -enable-kvm &
