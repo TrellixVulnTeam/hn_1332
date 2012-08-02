2de351b616a58a6097ec44751f00348e25b31c78
Laurent David <laurent@tdm.info>
2012-08-02 10:44:57 +0100

    Ready for client test

--- test/integration/runEmulator.sh
+++ test/integration/runEmulator.sh
@@ -8,11 +8,16 @@
 
 echo "Launch the VM to build HyperNova Package: will be use for the manager AND the client"
 echo "Running emulator. You can ssh into it from the port 3333"
-qemu-system-x86_64 -m 512 -net nic,macaddr=52:54:00:f5:80:66 -net user -hda node.img  -enable-kvm -redir tcp:3333::22 &
+qemu-system-x86_64 -m 512 -net nic,macaddr=52:54:00:f5:80:66 -net user -hda node.img  -enable-kvm -redir tcp:3333::22 -nographic &
+QEMU_PID=$!
 
 until `ssh -l hnbuild localhost -p 3333 exit` ; do echo -n "."; done
 
 
 scp -P 3333 -r RPMS/ hnbuild@localhost:/home/hnbuild/hypernova/
+
+expect -f installAgent.tcl
+
+#kill $QEMU_PID
 
 
