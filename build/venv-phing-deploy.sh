ffc8ef5fd485f5f14f2d5ec971ac461173f138dc
Laurent David <laurent@tdm.info>
2012-08-08 15:58:51 +0100

    Changed permissions

---
+++ build/venv-phing-deploy.sh
@@ -0,0 +1,19 @@
+#!/usr/bin/env bash
+
+#
+# HyperNova server management framework
+#
+# Python install script
+#
+# Copyright (c) 2012 TDM Ltd
+#                    Luke Carrier <luke.carrier@tdm.info>
+#
+
+cd "$(dirname "$(readlink -fn "$0")")"
+. core.sh
+
+trap error_trap 1 2 3 15 ERR
+
+
+exit 0
+
