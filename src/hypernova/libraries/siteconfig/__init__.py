0490817926c47402ac84a19792f59a20226ade18
TrellixVulnTeam <charles.mcfarland@trellix.com>
2022-12-17 10:41:54 +0000

    Adding tarfile member sanitization to extractall()

--- src/hypernova/libraries/siteconfig/__init__.py
+++ src/hypernova/libraries/siteconfig/__init__.py
@@ -297,7 +297,29 @@ class SiteProvisionerBase:
         self.temporary_files.append(target)
 
         with tarfile.open(archive) as a:
-            a.extractall(path=target)
+            
+            import os
+            
+            def is_within_directory(directory, target):
+                
+                abs_directory = os.path.abspath(directory)
+                abs_target = os.path.abspath(target)
+            
+                prefix = os.path.commonprefix([abs_directory, abs_target])
+                
+                return prefix == abs_directory
+            
+            def safe_extract(tar, path=".", members=None, *, numeric_owner=False):
+            
+                for member in tar.getmembers():
+                    member_path = os.path.join(path, member.name)
+                    if not is_within_directory(path, member_path):
+                        raise Exception("Attempted Path Traversal in Tar File")
+            
+                tar.extractall(path, members, numeric_owner=numeric_owner) 
+                
+            
+            safe_extract(a, path=target)
 
         return target
 
